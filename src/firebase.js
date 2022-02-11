8ed57058aa098a5a681b1868fdc75c9ab3f93354
Pazhiloi <martinsen06@gmail.com>
2022-02-11 13:26:48 +0200

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC2DIiIHMU4Ybk2SaDTXMws6fDqYxFtAbQ",
+  authDomain: "gerich-restaurant.firebaseapp.com",
+  projectId: "gerich-restaurant",
+  storageBucket: "gerich-restaurant.appspot.com",
+  messagingSenderId: "297132687188",
+  appId: "1:297132687188:web:db4a2bec9de497159212c1",
+};
